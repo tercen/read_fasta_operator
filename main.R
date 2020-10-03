e9dff9e573fe1c46f2f4318e38bdd60cefbed0d1
agouy <contact@agouy.com>
2020-10-03 21:22:10 +0000

    first commit

--- main.R
+++ main.R
@@ -1,9 +1,33 @@
 library(tercen)
 library(dplyr)
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
+doc_to_data = function(df){
+  filename = tempfile()
+  writeBin(ctx$client$fileService$download(df$documentId[1]), filename)
+  on.exit(unlink(filename))
+  
+  dat <- readLines(filename)
+  first.lines <- grep(">", dat)
+  seqs <- list()
+  for(i in 1:(length(first.lines) - 1)) {
+    seq <- dat[first.lines[i]:(first.lines[i+1] - 1)]
+    df_tmp <- c(name = seq[1], sequence = paste(seq[-1], sep="", collapse=""))
+    seqs[[i]] <- df_tmp
+  }
+  
+  df_out <- as.data.frame(do.call(rbind, seqs)) %>%
+    mutate(.ci= rep_len(df$.ci[1], nrow(.)))
+  return(df_out)
+}
+
+ctx = tercenCtx()
+
+if (!any(ctx$cnames == "documentId")) stop("Column factor documentId is required") 
+
+ctx$cselect() %>% 
+  mutate(.ci= 1:nrow(.)-1) %>%
+  split(.$.ci) %>%
+  lapply(doc_to_data) %>%
+  bind_rows() %>%
   ctx$addNamespace() %>%
   ctx$save()
--- workspace.R
+++ workspace.R
@@ -1,15 +1,36 @@
 library(tercen)
 library(dplyr)
 
-options("tercen.workflowId" = "wwww")
-options("tercen.stepId"     = "dddd")
+options("tercen.workflowId" = "a77770c3923fad0ca99b77fa8905471d")
+options("tercen.stepId"     = "c5813823-50ed-4f7b-ac6c-262b0aeb103d")
 
-getOption("tercen.workflowId")
-getOption("tercen.stepId")
+doc_to_data = function(df){
+  filename = tempfile()
+  writeBin(ctx$client$fileService$download(df$documentId[1]), filename)
+  on.exit(unlink(filename))
+  
+  dat <- readLines(filename)
+  first.lines <- grep(">", dat)
+  seqs <- list()
+  for(i in 1:(length(first.lines) - 1)) {
+    seq <- dat[first.lines[i]:(first.lines[i+1] - 1)]
+    df_tmp <- c(name = seq[1], sequence = paste(seq[-1], sep="", collapse=""))
+    seqs[[i]] <- df_tmp
+  }
+  
+  df_out <- as.data.frame(do.call(rbind, seqs)) %>%
+    mutate(.ci= rep_len(df$.ci[1], nrow(.)))
+  return(df_out)
+}
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
+ctx = tercenCtx()
+
+if (!any(ctx$cnames == "documentId")) stop("Column factor documentId is required") 
+
+ctx$cselect() %>% 
+  mutate(.ci= 1:nrow(.)-1) %>%
+  split(.$.ci) %>%
+  lapply(doc_to_data) %>%
+  bind_rows() %>%
   ctx$addNamespace() %>%
   ctx$save()
